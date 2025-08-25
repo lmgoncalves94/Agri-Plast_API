7042aee034d4f719c91a8912e6bfd7a489cbf5e9
Luís Miguel Gonçalves <65960293+lmgoncalves94@users.noreply.github.com>
2025-08-25 06:23:13 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Agri-Plast API Tutorial'
+copyright = '2025, Luís Miguel Gonçalves'
+author = 'Luís Miguel Gonçalves'
 
 release = '0.1'
 version = '0.1.0'
